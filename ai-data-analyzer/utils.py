7d4d7cfaab39860300115c3ac2cb353f78364ac6
Moustafa Mohamed <161119254+MoustafaMohamed01@users.noreply.github.com>
2025-05-18 23:48:26 +0300

    utils

---
+++ ai-data-analyzer/utils.py
@@ -0,0 +1,8 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain.chains.question_answering import load_qa_chain
+import os
+import google.generativeai as genai
+from langchain_google_genai import ChatGoogleGenerativeAI
