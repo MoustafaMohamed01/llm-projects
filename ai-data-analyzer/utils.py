25b0d5d890a41a484a137ec6a6753094ee68067f
Moustafa Mohamed <161119254+MoustafaMohamed01@users.noreply.github.com>
2025-05-18 23:53:30 +0300

    Update utils.py

--- ai-data-analyzer/utils.py
+++ ai-data-analyzer/utils.py
@@ -6,3 +6,7 @@ from langchain.chains.question_answering import load_qa_chain
 import os
 import google.generativeai as genai
 from langchain_google_genai import ChatGoogleGenerativeAI
+from api_key import GEMINI_API_KEY
+
+genai.configure(api_key=GEMINI_API_KEY)
+
