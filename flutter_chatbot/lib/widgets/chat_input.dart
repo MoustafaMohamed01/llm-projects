a71b94c7b427cab00411b8741d7b921ef08ebc44
Moustafa Mohamed <161119254+MoustafaMohamed01@users.noreply.github.com>
2025-09-16 00:08:04 +0300

    Add files via upload

---
+++ flutter_chatbot/lib/widgets/chat_input.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class ChatInput extends StatefulWidget {
+  final Function(String) onSendMessage;
+
+  const ChatInput({
+    super.key,
+    required this.onSendMessage,
+  });
+
+  @override
+  State<ChatInput> createState() => _ChatInputState();
+}
+
+class _ChatInputState extends State<ChatInput> {
+  final TextEditingController _controller = TextEditingController();
+  bool _canSend = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(_onTextChanged);
+  }
+
+  void _onTextChanged() {
+    setState(() {
+      _canSend = _controller.text.trim().isNotEmpty;
+    });
+  }
+
+  void _sendMessage() {
+    if (_canSend) {
+      widget.onSendMessage(_controller.text);
+      _controller.clear();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Theme.of(context).scaffoldBackgroundColor,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 4,
+            offset: const Offset(0, -2),
+          ),
+        ],
+      ),
+      child: SafeArea(
+        child: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                controller: _controller,
+                decoration: InputDecoration(
+                  hintText: 'Type a message...',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 12,
+                  ),
+                ),
+                maxLines: null,
+                textCapitalization: TextCapitalization.sentences,
+                onSubmitted: (_) => _sendMessage(),
+              ),
+            ),
+            const SizedBox(width: 8),
+            FloatingActionButton.small(
+              onPressed: _canSend ? _sendMessage : null,
+              backgroundColor: _canSend
+                  ? Theme.of(context).colorScheme.primary
+                  : Colors.grey[300],
+              child: Icon(
+                Icons.send,
+                color: _canSend ? Colors.white : Colors.grey[600],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
